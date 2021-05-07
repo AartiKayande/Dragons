3695ff8196d5018705d8bda3f6f55bcc11245a69
DESKTOP-UIT5GT6\DELL <aarti.andhale99@gmail.com>
2021-05-07 17:34:54 +0530

    Dragons project

---
+++ Assets/Scripts/DragonKiller.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DragonKiller : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > 5.74)
+            SceneManager.LoadScene(0);
+
+        if (transform.position.y < -5.42)
+            SceneManager.LoadScene(0);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+    }
+}
